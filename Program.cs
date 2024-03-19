9e7ba7052d118be94f9f40b8eab735652992dda2
mjvhudson <72444915+mjvhudson@users.noreply.github.com>
2024-03-19 19:41:49 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using RickAndMortyProject.App_Code.UmbracoEndpointBuilders;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+        u.UseCustomRouting();
+    });
+
+await app.RunAsync();
